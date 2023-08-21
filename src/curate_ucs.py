2f6aec84b32bd764603253d7169ee751d2792661
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2023-08-21 09:37:06 -0300

    Generate and curate some of the mappings

---
+++ src/curate_ucs.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import json
+from wdcuration import generate_curation_spreadsheet
+
+from pathlib import Path
+
+HERE = Path(__file__).parent.resolve()
+DATA = HERE.parent.joinpath("data").resolve()
+RESULTS = HERE.parent.joinpath("results").resolve()
+
+
+def main():
+    generate_curation_spreadsheet(
+        curation_table_path=DATA.joinpath("table_data21.csv"),
+        identifiers_property="P11954",
+        description_term_lookup="",
+        output_file_path="curation_sheet.csv",
+    )
+
+
+if __name__ == "__main__":
+    main()
